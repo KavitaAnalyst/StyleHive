d7e82f045d4acc0f62279854f9658dfc6bde13a7
Kavita tripathi <kaviitatripathi@gmail.com>
2024-05-24 11:40:55 +0530

    Removed default code from App.jsx file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/StyleHive/",
+  build:{
+    outDir:"dist"
+  },
   plugins: [react()],
 })
